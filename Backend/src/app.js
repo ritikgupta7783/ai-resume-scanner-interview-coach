81e6fa36073a84f73f1cce2942ff3d3b0a38fa53
Ritik Kumar Gupta <ritikgalaxy555@gmail.com>
2026-07-01 02:52:31 +0530

    Fix cors for deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-console.log("CLIENT_URL =", process.env.CLIENT_URL)
+
 app.use(
   cors({
     origin: [
@@ -15,20 +15,18 @@ app.use(
     ],
     credentials: true,
   })
-);
+)
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-
 /* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
+
 app.get("/", (req, res) => {
-    res.send("Backend Running 🚀")
+  res.send("Backend Running 🚀")
 })
-
-
 
 module.exports = app
